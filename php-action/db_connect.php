fa2d22a6bb9efb62d574126c30a0df8c7444412a
Deivisson-dev <darsonsilva83@gmail.com>
2024-04-18 21:49:12 -0300

    Add new PHP files and update existing files for CRUD functionality

---
+++ php-action/db_connect.php
@@ -0,0 +1,14 @@
+<?php 
+    // Conexão
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $db_name = "crud";
+
+    $connect = mysqli_connect($servername, $username, $password, $db_name);
+    mysqli_set_charset($connect, "utf8");
+
+    if(mysqli_connect_error()) {
+        echo "Falha na conexão: ".mysqli_connect_error();
+    }
+?>
